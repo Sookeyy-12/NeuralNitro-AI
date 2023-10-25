97d413249434027937cb9ca9465aaf9a30bf67b0
Sookeyy-12 <thatonesuket@gmail.com>
2023-10-25 20:23:31 +0530

    added behaviour script

--- Assets/Scripts/CarBehaviour.cs
+++ Assets/Scripts/CarBehaviour.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using System;
 
 public class CarBehaviour : Agent
 {
-    
+
+    public CarController controller;
+
+    public override void Initialize()
+    {
+        controller = GetComponent<CarController>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        
+    }
 }
